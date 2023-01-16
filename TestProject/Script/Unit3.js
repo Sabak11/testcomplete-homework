7c1e0afe1bd669a42a11da160002ef15e5aa6f90
Sabak11 <metertmeteli@gmail.com>
2023-01-17 02:37:25 +0400

    Upload DDT Files

--- TestProject/Script/Unit3.js
+++ TestProject/Script/Unit3.js
@@ -1,1 +1,11 @@
-﻿
+﻿function getConnection()
+{
+ // Specifies the connection string
+  var ConStr = "Provider=MSOLEDBSQL;Server=localhost;Database=orders;UID=testAutomation;PWD=Testautomation123;";
+  var connection = ADO.CreateConnection();
+  connection.ConnectionString = ConStr;
+  connection.Open();
+  return connection;
+}
+
+module.exports.getConnection=getConnection
---
+++ TestProject/Script/Unit5.js
@@ -0,0 +1,48 @@
+﻿
+
+function Test1()
+{
+  
+TestedApps.Orders.Run(1, true);
+  
+  Project.Variables.Data.Reset();
+  
+  for(; !Project.Variables.Data.IsEOF();){
+    
+  var Values = Project.Variables.Data.Value;
+  
+  
+  
+
+  let orders = Aliases.Orders;
+  let mainForm = orders.MainForm;
+  let toolBar = mainForm.ToolBar;
+  toolBar.ClickItem(4, false);
+  let orderForm = orders.OrderFormOne;
+  let groupBox = orderForm.Group;
+  let textBox = groupBox.Customer;
+  textBox.Click(27, 5);
+  textBox.SetText(Values("customerName"));
+  textBox = groupBox.Street;
+  textBox.Click(36, 6);
+  textBox.SetText(Values("street"));
+  textBox = groupBox.Town;
+  textBox.Click(16, 11); 
+  textBox.SetText(Values("state"));
+  textBox = groupBox.State;
+  textBox.DblClick(101, 17);
+  textBox.SetText(Values("state"));
+  orderForm.ButtonOK.ClickButton();
+  toolBar.ClickItem(6, false);
+  let dlgConfirmation = orders.dlgConfirmation;
+  dlgConfirmation.Click(95, 110);
+  dlgConfirmation.btnYes.ClickButton();
+  mainForm.Close();
+  dlgConfirmation.btnNo.ClickButton();
+  
+   Project.Variables.Data.Next()
+
+}
+}
+
+ 
